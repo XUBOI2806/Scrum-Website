0b76d4e552e668ea50fb0593f31bc43453a5a12d
YOUR_NAME_HERE <fxuu0010@student.monash.edu>
2022-09-01 16:32:30 +1000

    adding fromData function for Task class

--- Projects/js/shared.js
+++ Projects/js/shared.js
@@ -106,6 +106,16 @@ class Task {
         this._tags = tags;
         this._timeTracking = timeTracking;
     }
+
+    fromData(data) {
+        this._title = data._title;
+        this._description = data._description;
+        this._status = data._status;
+        this._priority = data._priority;
+        this._assigned = data._assigned;
+        this._tags = data._tags;
+        this._timeTracking = data._timeTracking;
+    }
 }
 
 class Person {
@@ -133,22 +143,29 @@ class Person {
     set email(value) {
         this._email = value;
     }
+    fromData(data) {
+        this._name = data._name;
+        this._email = data._email;
+    }
 }
 
 class Backlog {
     constructor() {
-        this._taskArray = [];
+        this._array = [];
         if (this.constructor === Backlog) {
             throw new Error("Abstract class cannot be instantiated");
         }
     }
 
     addTask(newTask) {
-        this._taskArray.push(newTask);
+        this._array.push(newTask);
     }
 
     deleteTask(taskIndex) {
         throw new Error("Method deleteTask must be implemented");
+    }
+
+    fromData(data) {
     }
 }
 
@@ -158,12 +175,21 @@ class SprintBacklog extends Backlog{
     }
 
     addTask(newTask){
-        this._taskArray.push(newTask);
+        this._array.push(newTask);
     }
 
     deleteTask(taskIndex){
-        this._taskArray.splice(taskIndex, 1)
+        this._array.splice(taskIndex, 1)
     }
+
+    // fromData(data) {
+    //     this._array = [];
+    //     for (let i = 0; i < data._array.length; i++) {
+    //         let sprint = new Sprint();
+    //         sprint.fromData(data._array[i]);
+    //         this._array.push(sprint);
+    //     }
+    // }
 }
 
 class ProductBacklog extends Backlog{
@@ -172,11 +198,20 @@ class ProductBacklog extends Backlog{
     }
 
     addTask(newTask){
-        this._taskArray.push(newTask);
+        this._array.push(newTask);
     }
 
     deleteTask(taskIndex){
-        this._taskArray.splice(taskIndex, 1)
+        this._array.splice(taskIndex, 1)
+    }
+
+    fromData(data) {
+        this._array = [];
+        for (let i = 0; i < data._array.length; i++) {
+            let task = new Task();
+            task.fromData(data._array[i]);
+            this._array.push(task);
+        }
     }
 }
 
@@ -195,6 +230,7 @@ function checkLSData(key) {
     }
     return false;
 }
+
 /**
  * retrieveLSData function
  * Used to retrieve data from LS at a specific key.
@@ -210,6 +246,7 @@ function retrieveLSData(key) {
         return data;
     }
 }
+
 /**
  * updateLSData function
  * Used to store JS data in LS at a specific key
