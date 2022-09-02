b862fd5085d748548b6b481b5b791e4a9ab89019
YOUR_NAME_HERE <fxuu0010@student.monash.edu>
2022-09-02 22:53:09 +1000

    Adding createTeamMember, deleteTeamMember, displayTeamBacklog function

--- Projects/js/Team/team.js
+++ Projects/js/Team/team.js
@@ -1,11 +1,51 @@
+
 "use strict";
 
-import { Person } from "../shared";
-
 function createTeamMember() {
-    // Take in use inputs
+    // take in user inputs
     let name = document.getElementById("memberName").value;
     let email = document.getElementById("memberEmail").value;
 
-    let teamMember = new Person(name, email)
+    let person = new Person(name, email);
+    updateLSData(PERSON_KEY, person)
+    teamBacklog.addTask(person);
+    updateLSData(TEAMBACKLOG_KEY, teamBacklog)
+    displayTeamBacklog();
+    closeDialog();
 }
+
+function deleteTeamMember(index){
+    teamBacklog.deleteTask(index);
+    displayTeamBacklog();
+}
+
+function displayTeamBacklog() {
+    let output = "";
+    for (let i = 0; i < teamBacklog._array.length; i++) {
+        let item = `<ul class="mdl-list">
+                <li class="PBI mdl-list__item mdl-list__item--three-line">
+                    <span class="mdl-list__item-primary-content">
+                        <span>${productBacklog._array[i]._title}</span>
+                        <span class="mdl-list__item-text-body">${productBacklog._array[i]._description}</span>
+                    </span>
+                    <span class="mdl-list__item-secondary-content">
+                        <!-- Colored icon button -->
+                        <button class="mdl-button mdl-js-button mdl-button--icon mdl-button--colored">
+                            <i class="material-icons">edit</i>
+                        </button>
+                    </span>
+                    <span class="mdl-list__item-secondary-content">
+                        <!-- Colored icon button -->
+                        <button class="mdl-button mdl-js-button mdl-button--icon mdl-button--colored" onclick="deleteTask(${i})">
+                            <i class="material-icons">delete</i>
+                        </button>
+                    </span>
+                </li>
+            </ul>`;
+        output += item;
+    }
+    document.getElementById("content").innerHTML = output;
+}
+
+// Displays the list of vacations when the page loads
+displayTeamBacklog();
