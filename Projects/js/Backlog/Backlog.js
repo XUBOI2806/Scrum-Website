bb31dbd70f0d29c7d0c5233f27e860c24005e273
mailybutel <mailybutel@gmail.com>
2022-09-06 21:19:16 +1000

    Validation for creating new task inputs

--- Projects/js/Backlog/Backlog.js
+++ Projects/js/Backlog/Backlog.js
@@ -4,6 +4,9 @@
 // let taskIndex = localStorage.getItem(TASK_KEY);
 // let selectedTask = productBacklog.getTask(taskIndex);
 
+/**
+ * Create a new task and add it to local storage
+ */
 function createTask() {
   // take in user inputs
   let name = document.getElementById("pbiName").value;
@@ -15,8 +18,10 @@ function createTask() {
   let effort = document.getElementById("pbiEffort").value;
   let tagCheckboxes = document.querySelectorAll('input[name="tag"]:checked');
 
+  // Create task
   let persons = new Person(person, "asfda");
   let task = new Task(name, des, status, priority, person, effort);
+  // Get the checked tags
   tagCheckboxes.forEach((checkbox) => {
     switch (checkbox.value){
       case "UI":
@@ -33,33 +38,52 @@ function createTask() {
     }
   });
 
-  updateLSData(TASK_KEY, task)
-  productBacklog.addTask(task);
-  updateLSData(PRODUCTBACKLOG_KEY, productBacklog)
-  displayProductBacklog();
-  // reset the input field to empty
-  document.getElementById("pbiName").value = "";
-  document.getElementById("pbiDesc").value = "";
-  document.getElementById("pbiType").value = "";
-  document.getElementById("person").value = "";
-  document.getElementById("priority").value = "";
-  document.getElementById("status").value = "";
-  document.getElementById("pbiEffort").value = "";
+  // Check that all inputs are valid
+  if(validateInputs(name, des, taskType, person, priority, status, effort)){
+    // Add to local storage
+    updateLSData(TASK_KEY, task)
+    productBacklog.addTask(task);
+    updateLSData(PRODUCTBACKLOG_KEY, productBacklog)
+    // Update Task list
+    displayProductBacklog();
+    // reset the input field to empty
+    document.getElementById("pbiName").value = "";
+    document.getElementById("pbiDesc").value = "";
+    document.getElementById("pbiType").value = "";
+    document.getElementById("person").value = "";
+    document.getElementById("priority").value = "";
+    document.getElementById("status").value = "";
+    document.getElementById("pbiEffort").value = "";
+    document.getElementById("tag-ui").parentElement.classList.remove("is-checked");
+    document.getElementById("tag-dev").parentElement.classList.remove("is-checked");
+    document.getElementById("tag-test").parentElement.classList.remove("is-checked");
+    tagCheckboxes.forEach((checkbox) => {
+      checkbox.checked = false;
+    });
 
-  document.getElementById("saveTask").removeEventListener("click", createTask);
-  closeDialog();
+    // close dialog
+    document.getElementById("saveTask").removeEventListener("click", createTask);
+    closeDialog();
+  }
 }
 
+/**
+ * Delete a task
+ */
 function deleteTask(index){
     productBacklog.delete(index)
     displayProductBacklog()
 }
 
+/**
+ * Show all Tasks in a list
+ */
 function displayProductBacklog() {
   let output = "";
-  for (let i = 0; i < productBacklog._taskArray.length; i++) {
 
-    // Get tags
+  // Iterate through saved tasks in the backlog
+  for (let i = 0; i < productBacklog._taskArray.length; i++) {
+    // Get tags of each task
     let tagsOutput = "";
     productBacklog._taskArray[i].tags.forEach((tag) => {
       tagsOutput += `
@@ -69,7 +93,7 @@ function displayProductBacklog() {
       `;
     })
 
-
+    // Create html to display the task info
     let item = `
                 <li class="PBI mdl-list__item mdl-list__item--three-line">
                     <span class="mdl-list__item-primary-content">
@@ -97,10 +121,14 @@ function displayProductBacklog() {
               </li>`;
     output += item;
   }
+  // Add to the UI list
   document.getElementById("pbi-list").innerHTML = output;
 
 }
 
+/**
+ * Put saved values of task into the dialog container
+ */
 function editTask(index) {
   let name = document.getElementById("pbiName");
   name.value = productBacklog._taskArray[index]._name;
@@ -114,9 +142,11 @@ function editTask(index) {
   assigned.value = productBacklog._taskArray[index]._assigned;
   let timeTracking = document.getElementById("pbiEffort");
   timeTracking.value = productBacklog._taskArray[index]._timeTracking;
-
 }
 
+/**
+ * Update the modified task
+ */
 function saveEditTask(index){
   let name = document.getElementById("editName").value;
   let description = document.getElementById("editDescription").value;
@@ -131,7 +161,65 @@ function saveEditTask(index){
   closeDialog();
 }
 
+/**
+ * Show a task in the dialog container with uneditable inputs
+ */
+function showTask(){
+  let name = document.getElementById("pbiName");
+  name.value = productBacklog._taskArray[index]._name;
+  let description = document.getElementById("pbiDesc");
+  description.value = productBacklog._taskArray[index]._description;
+  let status = document.getElementById("status");
+  status.value = productBacklog._taskArray[index]._status;
+  let priority = document.getElementById("priority");
+  priority.value = productBacklog._taskArray[index]._priority;
+  let assigned = document.getElementById("person");
+  assigned.value = productBacklog._taskArray[index]._assigned;
+  let timeTracking = document.getElementById("pbiEffort");
+  timeTracking.value = productBacklog._taskArray[index]._timeTracking;
+}
 
+/**
+ * Check that all inputs are valid, otherwise show error messages
+ */
+function validateInputs(name, desc, type, person, priority, status, effort){
+  let retVal = true;
+
+  if(name === ''){
+    document.getElementById("pbiName").parentElement.classList.add("is-invalid");
+    retVal = false;
+  }
+  if(desc === ''){
+    document.getElementById("pbiDesc").parentElement.classList.add("is-invalid");
+    retVal = false;
+  }
+  if(type === ''){
+    document.getElementById("pbiType").parentElement.classList.add("is-invalid");
+    retVal = false;
+  }
+  if(person === ''){
+    document.getElementById("person").parentElement.classList.add("is-invalid");
+    retVal = false;
+  }
+  if(priority === ''){
+    document.getElementById("priority").parentElement.classList.add("is-invalid");
+    retVal = false;
+  }
+  if(status === ''){
+    document.getElementById("status").parentElement.classList.add("is-invalid");
+    retVal = false;
+  }
+  if(effort === ''){
+    document.getElementById("pbiEffort").parentElement.classList.add("is-invalid");
+    retVal = false;
+  }
+
+  return retVal;
+}
+
+/**
+ * Open up the dialog so that a new task can be created
+ */
 function add_pbi(){
   let dialog = document.querySelector('dialog');
   if (!dialog.showModal()){
@@ -140,11 +228,17 @@ function add_pbi(){
   }
 }
 
+/**
+ * Close the dialog
+ */
 function closeDialog(){
   let dialog = document.querySelector('dialog');
   dialog.close();
 }
 
+/**
+ * Open the dialog so that a task can be edited
+ */
 function edit_pbi(){
   let dialog = document.querySelector('dialog');
   if (!dialog.showModal()){
