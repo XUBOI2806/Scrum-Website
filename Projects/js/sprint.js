61baef1182bb655ef62fedecc596f0f6203bbe4e
Jas <jbha0006@student.monash.edu>
2022-09-18 20:00:11 +1000

    creating sprints

--- Projects/js/sprint.js
+++ Projects/js/sprint.js
@@ -2,11 +2,48 @@
 /**
  * Open the add sprint dialog
  */
+let nameSprint = []
+
 function add_sprint(){
     let dialog = document.querySelector("dialog");
     dialog.showModal();
     dialogPolyfill.registerDialog(dialog);
 }
+
+function createSprint(){
+        // take in user inputs
+    let sprintName = document.getElementById("sprintName").value;
+    let startDate = document.getElementById("startDate").value;
+    let endDate = document.getElementById("endDate").value;
+    nameSprint.push(sprintName)
+    displaySprint()
+    closeDialog()
+}
+
+
+function displaySprint() {
+    let output = "";
+  
+    // Iterate through saved tasks in the backlog
+    for (let i = 0; i < nameSprint.length; i++) {
+  
+      // Create html to display the task info
+      let item = `
+        <li class="list-item mdl-list__item" onclick="showManageSprint(0)">
+            <span class="mdl-list__item-primary-content" onclick="showTask()">
+                <span>${nameSprint[i]}</span>
+            </span>
+            <span class="mdl-list__item-secondary-content">
+                <!-- Edit button -->
+                <span>Status</span>
+             </span>
+        </li>`;
+      output += item;
+    }
+    // Add to the UI list
+    document.getElementById("sprint-list").innerHTML = output;
+  }
+  
 
 /**
  * Close the dialog
