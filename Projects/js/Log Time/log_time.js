fb40b7452b5e0b1ff1e9cacf89db82dc3912447a
Ethan Owens <eowe0002@student.monash.edu>
2022-10-04 15:33:56 +1100

    Fixed logging time to input tuples in Person class

--- Projects/js/Log Time/log_time.js
+++ Projects/js/Log Time/log_time.js
@@ -19,11 +19,10 @@ function addEffort(name) {
     if (person === undefined){
         return
     }
-    let newEffort = parseFloat(document.getElementById("loggedEffort").value) + person.loggedTime;
-    person.loggedTime = newEffort;
+    // **Update task total time**
+    let loggedTime = (document.getElementById("logDate").value, document.getElementById("loggedEffort").value)
+    person.addLoggedTime(loggedTime)
     updateLSData(TEAMBACKLOG_KEY, teamBacklog);
-
-    document.getElementById("totalHours").innerText = newEffort + " Hours";
 }
 
 function displayPage() {
@@ -38,7 +37,7 @@ function displayPage() {
                             <span>${tempTask.title}</span>
                             <h4>Description</h4>
                             <span>${tempTask.description}</span>
-                            <h4>Persons Assigned</h4>
+                            <h4>Person Assigned</h4>
                             <ul class="persons-list demo-list-two mdl-list">
                                 <li class="mdl-list__item mdl-list__item--two-line">
                                     <span class="mdl-list__item-primary-content">
@@ -54,13 +53,18 @@ function displayPage() {
                         <form action="#">
                             <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
                                 <input class="mdl-textfield__input" type="text" pattern="-?[0-9]*(\.[0-9]+)?" id="loggedEffort">
-                                <label class="mdl-textfield__label" for="sample2">Hours</label>
+                                <label class="mdl-textfield__label" for="sample2">Story Points</label>
                                 <span class="mdl-textfield__error">Input is not a number</span>
                             </div>
                         </form>
+                        <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label has-placeholder" style="width: 49%">
+                            <input class="mdl-textfield__input" type="date" id="logDate" placeholder="DD/MM/YYYY">
+                            <label class="mdl-textfield__label" for="logDate">Log Date</label>
+                            <span id="startDateError" class="mdl-textfield__error">Start Date today or before required</span>
+                        </div>
                         <button class="mdl-button mdl-js-button mdl-button--raised" onclick="addEffort('${tempTask.assigned.name}')">Add</button>
                         <h3>Total Time</h3>
-                        <span id = "totalHours">${tempTask.timeTracking} Hours</span>
+                        <span id="totalHours">${tempTask.timeTracking} Hours</span>
                     </div>
                 </div>`;
     document.getElementById("log-time-content").innerHTML = page;
