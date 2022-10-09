c8280458a9aefa045a8972efd1084d99b0c84ee0
Ethan Owens <eowe0002@student.monash.edu>
2022-10-10 10:53:11 +1100

    Fixed displaying of team member's total effort Changed layout of page

--- Projects/js/persons_time.js
+++ Projects/js/persons_time.js
@@ -2,6 +2,18 @@
 
 function displayPage() {
     let temp = new Person("Temp Name", "temp@gmail.com");
+    temp.addLoggedTime(['14/10/2022', 7])
+    temp.addLoggedTime(['11/10/2022', 2])
+    temp.addLoggedTime(['14/10/2022', 7])
+    temp.addLoggedTime(['11/10/2022', 2])
+    temp.addLoggedTime(['14/10/2022', 7])
+    temp.addLoggedTime(['11/10/2022', 2])
+    temp.addLoggedTime(['14/10/2022', 7])
+    temp.addLoggedTime(['11/10/2022', 2])
+    temp.addLoggedTime(['14/10/2022', 7])
+    temp.addLoggedTime(['11/10/2022', 2])
+    temp.addLoggedTime(['14/10/2022', 7])
+    temp.addLoggedTime(['11/10/2022', 2])
 
     // Collecting all logged time in to html
     let logged_time = ``;
@@ -9,10 +21,14 @@ function displayPage() {
     for (let i = 0; i < temp.loggedTime.length; i++) {
         logged_time += `
                         <li class="list-item mdl-list__item mdl-list__item--one-line">
-                            <span class="mdl-list__item-primary-content">
-                                <span>${temp.loggedTime[i][0]}</span>
-                                <span style="float: right">${temp.loggedTime[i][1].toString()} Story Points</span>
-                            </span>
+                            <div id="persons-effort-item">
+                                <div>
+                                    <span class="mdl-list__item-primary-content">${temp.loggedTime[i][0]}</span>
+                                </div>
+                                <div id="right">
+                                    <span id="right" class="persons-logged-time mdl-list__item-secondary-content">${temp.loggedTime[i][1].toString()} Story Points</span>
+                                </div>
+                            </div>
                         </li>`
         total_effort += temp.loggedTime[i][1]
     }
@@ -22,10 +38,12 @@ function displayPage() {
                 <div class="content-grid mdl-grid">
                     <div class="mdl-cell mdl-cell--3-col"></div>
                     <div class="mdl-cell mdl-cell--6-col">
-                        <h4>${temp.name}</h4>
-                        <ul class="mdl-list">${logged_time}
-                        </ul>
-                        <span><h3>Total Effort: ${total_effort.toString()} Story Points</h3></span>
+                        <h3 style="color:#666666">${temp.name}</h3>
+                        <span style="text-align: center"><h3>Total Effort: ${total_effort.toString()} Story Points</h3></span>
+                        <div class="persons-effort list-container">
+                            <ul id="logged-time-list" class="mdl-list">${logged_time}
+                            </ul>
+                        </div>
                     </div>
                 </div`;
     document.getElementById("persons-time-content").innerHTML = page;
