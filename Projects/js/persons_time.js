b4478e60f110ba0b5cfecd1ad9f641a609786a88
Jas <jbha0006@student.monash.edu>
2022-10-10 11:14:44 +1100

    using people from team backlog for data demo

--- Projects/js/persons_time.js
+++ Projects/js/persons_time.js
@@ -1,27 +1,28 @@
 "use strict";
 
 function displayPage() {
-    let temp = new Person("Team Members Name", "temp@gmail.com");
-    temp.addLoggedTime(['~Test~ 14/10/2022', 7])
-    temp.addLoggedTime(['~Test~ 14/10/2022', 2])
+    let person = teamBacklog._array[0]
+    console.log(teamBacklog._array[0])
+    person.addLoggedTime(['~Test~ 14/10/2022', 7])
+    person.addLoggedTime(['~Test~ 14/10/2022', 2])
 
 
     // Collecting all logged time in to html
     let logged_time = ``;
     let total_effort = 0;
-    for (let i = 0; i < temp.loggedTime.length; i++) {
+    for (let i = 0; i < person.loggedTime.length; i++) {
         logged_time += `
                         <li class="list-item mdl-list__item mdl-list__item--one-line">
                             <div id="persons-effort-item">
                                 <div>
-                                    <span class="mdl-list__item-primary-content">${temp.loggedTime[i][0]}</span>
+                                    <span class="mdl-list__item-primary-content">${person.loggedTime[i][0]}</span>
                                 </div>
                                 <div id="right">
-                                    <span id="right" class="persons-logged-time mdl-list__item-secondary-content">${temp.loggedTime[i][1].toString()} Story Points</span>
+                                    <span id="right" class="persons-logged-time mdl-list__item-secondary-content">${person.loggedTime[i][1].toString()} Story Points</span>
                                 </div>
                             </div>
                         </li>`
-        total_effort += temp.loggedTime[i][1]
+        total_effort += person.loggedTime[i][1]
     }
 
 
@@ -29,7 +30,7 @@ function displayPage() {
                 <div class="content-grid mdl-grid">
                     <div class="mdl-cell mdl-cell--3-col"></div>
                     <div class="mdl-cell mdl-cell--6-col">
-                        <h3 style="color:#666666">${temp.name}</h3>
+                        <h3 style="color:#666666">${person.name}</h3>
                         <span style="text-align: center"><h3>Total Effort: ${total_effort.toString()} Story Points</h3></span>
                         <div class="persons-effort list-container">
                             <ul id="logged-time-list" class="mdl-list">${logged_time}
