c37a51034dfc1bccc43b91f419e70d573940b606
Jas <jbha0006@student.monash.edu>
2022-10-12 19:34:24 +1100

    getting total story points and converting into hrs

--- Projects/js/burn-down.js
+++ Projects/js/burn-down.js
@@ -42,3 +42,18 @@ new Chart(document.getElementById("bd-chart"), {
         }
     }
 });
+
+function idealV(){
+    let totalSP = 0
+
+    for(let i = 0; i< sprintBacklog._array.length; i++){
+        if(sprintBacklog._array[i].status == "In Progress"){
+            let sprintT = sprintBacklog._array[i].tasks
+            for(let j = 0; j<sprintT.length; j++){
+                console.log(sprintT[j].timeTracking)
+                totalSP +=  parseInt(sprintT[j].timeTracking)
+            }
+        }
+    }
+    let totalHrs = totalSP * 4  
+}
