bf5ded1885da52fa8958cc23eeae73740284db15
Jas <jbha0006@student.monash.edu>
2022-10-13 19:15:21 +1100

    burn down chart working for inprogress sprint

--- Projects/js/burn-down.js
+++ Projects/js/burn-down.js
@@ -1,6 +1,10 @@
-
 let labelArrayIV = [];
 let dataArrayIV = [];
+let dataER = [];
+let dataArrayAV = [];
+let currentSprint = null;
+let totalHrs = 0
+
 idealV();
 function idealV() {
   let totalSP = 0;
@@ -8,32 +12,53 @@ function idealV() {
 
   for (let i = 0; i < sprintBacklog._array.length; i++) {
     if (sprintBacklog._array[i].status == "In Progress") {
+        currentSprint = sprintBacklog._array[i];
       let sprintT = sprintBacklog._array[i].tasks;
       let numTime =
         new Date(sprintBacklog._array[i]._endDate).getTime() -
         new Date(sprintBacklog._array[i].startDate).getTime();
       numDays = numTime / (1000 * 3600 * 24);
-      console.log(numDays);
       for (let j = 0; j < sprintT.length; j++) {
         console.log(sprintT[j]._effort);
         totalSP += parseInt(sprintT[j]._effort);
       }
     }
-}
+  }
   for (let i = 0; i <= numDays; i++) {
     labelArrayIV.push(i + 1);
+    dataER.push(0)
+    dataArrayAV.push(0)
   }
-  let totalHrs = totalSP * 4;
+  totalHrs = totalSP * 4;
   let avgDay = totalHrs / numDays;
   dataArrayIV.push(totalHrs);
+  let hours = totalHrs
   while (dataArrayIV.length != numDays) {
-    let val = totalHrs - avgDay;
+    let val = hours - avgDay;
     dataArrayIV.push(val);
-    totalHrs = val;
+    hours = val;
   }
   dataArrayIV.push(0);
 }
 
+actualV()
+function actualV() {
+    for(let i = 0; i < currentSprint._tasks.length; i++){
+        let currentTask = currentSprint._tasks[i]._timeTracking
+        for(let j = 0; j < currentTask.length; j++){
+            let date = new Date(currentTask[j][0]).getTime()
+            let timeDate = date - new Date(currentSprint._startDate).getTime()
+            let numDate = timeDate / (1000 * 3600 * 24);
+            dataER[numDate] = parseInt(currentTask[j][1])
+        }
+    }
+    let remainingE = totalHrs
+    for(let i = 0; i < dataArrayAV.length; i++){
+        dataArrayAV[i] = remainingE - dataER[i]
+        remainingE = dataArrayAV[i]
+    }
+    console.log(dataArrayAV)
+}
 // timeTracking = (date, hr)
 
 new Chart(document.getElementById("bd-chart"), {
@@ -48,6 +73,20 @@ new Chart(document.getElementById("bd-chart"), {
         fill: false,
         lineTension: 0,
       },
+      {
+        data: dataER,
+        label: "Remaining Effort",
+        borderColor: "#7cc25e",
+        fill: false,
+        lineTension: 0,
+    },
+    {
+        data: dataArrayAV,
+        label: "Actual Velocity",
+        borderColor: "#8e5ea2",
+        fill: false,
+        lineTension: 0,
+    }
     ],
   },
   options: {
