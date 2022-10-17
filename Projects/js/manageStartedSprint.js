8749ef654a0e8df7ddc195092d11477696cd16ee
Felix Xu <fxuu0010@student.monash.edu>
2022-10-17 17:03:16 +1100

    - fixing manage sprint merge issue

--- Projects/js/manageStartedSprint.js
+++ Projects/js/manageStartedSprint.js
@@ -21,18 +21,19 @@ function displayTasks() {
     // Iterate through saved tasks in the backlog
     for (let i = 0; i < sprintBacklog._array[sprintKey]._tasks.length; i++) {
         // Create html to display the task info
-        let item = `
-                <li class="list-item mdl-list__item mdl-list__item">
-                    <span class="mdl-list__item-primary-content">
-                        <span>${sprintBacklog._array[sprintKey]._tasks[i]._title}</span>
-                    </span>
-                    <span class="mdl-list__item-secondary-content">
-                        <button class="mdl-button mdl-js-button mdl-button--icon mdl-button--colored" id="move-ns${i}" onclick="moveTaskToInProgress(${i})">
-                            <i class="material-icons">arrow_forward</i>
-                        </button>
-                        <div class="mdl-tooltip" data-mdl-for="move-ns${i}">Move to In Progress</div>
-                    </span>
-                </li>`
+        if(sprintBacklog._array[sprintKey]._tasks[i]._status === "In Progress"){
+            let item = `
+                    <li class="list-item mdl-list__item mdl-list__item">
+                        <span class="mdl-list__item-primary-content">
+                            <span>${sprintBacklog._array[sprintKey]._tasks[i]._title}</span>
+                        </span>
+                        <span class="mdl-list__item-secondary-content">
+                            <button class="mdl-button mdl-js-button mdl-button--icon mdl-button--colored" id="move-ns${i}" onclick="moveTaskToInProgress(${i})">
+                                <i class="material-icons">arrow_forward</i>
+                            </button>
+                            <div class="mdl-tooltip" data-mdl-for="move-ns${i}">Move to In Progress</div>
+                        </span>
+                    </li>`
             notStartedOutput += item;
         }
         else if(sprintBacklog._array[sprintKey]._tasks[i]._status === "In Progress"){
